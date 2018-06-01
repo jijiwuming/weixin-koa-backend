364d1495fa7c58f9d3803e90e39a0217eea03c51
铭 <1020293664@qq.com>
2018-06-01 18:05:32 +0800

    暂存了

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 const koa = require('koa')
+const Router = require('koa-router')
+const router = new Router()
+const bodyParser = require('koa-bodyparser')
 const app = new koa()
-const main = ctx => {
-  ctx.response.body = 'Hello World'
-}
-app.use(main)
+
+// log request URL:
+app.use(async (ctx, next) => {
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
+  await next()
+})
+
+router.get('/', (ctx, next) => {
+  if (ctx.query) {
+    let data = ctx.query.data
+    try {
+      let obj = JSON.parse(data)
+      console.dir(obj.a)
+    } catch (e) {
+      console.dir(e)
+    }
+  }
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+
 app.listen(3000)
